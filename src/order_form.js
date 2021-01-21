09d1c218893bec90244bb57ec171762ac7002362
Leif Tukums <leiftukums@gmail.com>
2021-01-21 07:22:35 -0500

    organized inputs

--- src/order_form.js
+++ src/order_form.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Order_form = () => {
-
+    const [clientOrder, setClientOrder] = useState({
+        name: '',
+        email: '',
+        eventDate: '',
+        eventTime: '',
+        eventLocation: '',
+        package: '',
+        additionalNotes: ''
+    })
 
     return (
         <div>
-            <div class='gallery'>
-                <h1>Package images will appear here</h1>
-            </div>
             <form class='selection-form'>
                 <label>Select from the following packages: </label>    
-                <select>
-                    <option>Saltwater Slam</option>
-                    <option>Be a little shellfish</option>
-                    <option>Green Paradise</option>
-                    <option>Chef's Catch</option>
-                    <option>Nibblers</option>
+                <select name='package'>
+                    <option value='Saltwater Slam'>Saltwater Slam</option>
+                    <option value='Be a little shellfish'>Be a little shellfish</option>
+                    <option value='Green Paradise'>Green Paradise</option>
+                    <option value="Chef's Catch">Chef's Catch</option>
+                    <option value='Nibblers'>Nibblers</option>
                 </select>
-                <div>
+                <div class='order_inputs'>
+                    <label>Your Name:</label>
+                    <input type='text' name='name'/>
+                    <label>Email:</label>
+                    <input type='email' name='email'></input>
                     <label>Date of Event:</label>
-                    <input type='text'/>
+                    <input type='text' name='eventDate'/>
                     <label>Time of Event:</label>
-                    <input type='text'/>
+                    <input type='text' name='eventTime'/>
                     <label>Location:</label>
-                    <input type='text' />
+                    <input type='text' name='eventLocation'/>
                     <label>Additional Notes:</label>
-                    <textarea/>
+                    <textarea name='additionalNotes'/>
                 </div>
+                    <button type='submit'>Submit Order</button>
             </form>
         </div>
     )
